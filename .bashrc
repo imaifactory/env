c42b0a22e06759a39d6de471a4fcb4769ba1444c
yuta imai <imai.factory@gmail.com>
2012-08-13 00:30:52 +0000

    commit for duplicate instance

--- .bashrc
+++ .bashrc
@@ -7,3 +7,10 @@ fi
 
 # User specific aliases and functions
 PATH=$PATH:~/aws/emrclient:~/aws/bin
+PERL5LIB=~/perl5/lib/perl5
+
+#setup aws environment
+#export AWS_ACCESS_KEY_ID=`perl ~/aws/bin/fetch_iam_data.pl AccessKeyId`
+#export AWS_SECRET_ACCESS_KEY=`perl ~/aws/bin/fetch_iam_data.pl SecretAccessKey`
+export AWS_ACCESS_KEY_ID=`AKIAJPZIPDOMDTQY55UA`
+export AWS_SECRET_ACCESS_KEY=`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`
--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
+yum -y update
+
 #setup ruby
-yum install gcc make libxml2-devel libxslt-devel
-yum install ruby-devel rubygems
+yum -y install gcc make libxml2-devel libxslt-devel
+yum -y install ruby-devel rubygems
 gem install nokogiri
 gem install aws-sdk
 
 #setup php
-yum install php php-pear php-xml
+yum -y install php php-pear php-xml
 pear channel-discover pear.amazonwebservices.com
 pear install aws/sdk
 
 #setup ruby
-yum install python-setuptools
+yum -y install python-setuptools
 easy_install pip
 pip install -U boto
