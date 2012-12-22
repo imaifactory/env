0b19af9d0b154e2c0c1c80715adf801b1a42823a
EC2 Default User <ec2-user@ip-10-146-97-99.ap-northeast-1.compute.internal>
2012-12-22 02:30:04 +0000

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+cp .vimrc ~/.vimrc
+echo "Installed ~/.vimrc"
+
+cp .screenrc ~/.screenrc
+echo "Installed ~/.screenrc"
+
+cp .bashrc ~/.bashrc
+echo "Installed ~/.bashrc"
+
+mkdir ~/.vim
+echo "Created ~/.vim"
+
+git clone https://github.com/Shougo/neocomplcache.git ~/.vim
+echo "Installed neocomplcache"
