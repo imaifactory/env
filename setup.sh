c73f45dd7bc9fd0b08e8befefd8575b238b2f40c
yuta <imai.factory@gmail.com>
2013-01-06 16:05:06 +0900

    s3cmd

--- setup.sh
+++ setup.sh
@@ -7,9 +7,17 @@ echo "Installed ~/.screenrc"
 cp -f .bashrc ~/.bashrc
 echo "Installed ~/.bashrc"
 
+cp -f .gitconfig ~/.gitconfig
+echo "Installed ~/.gitconfig"
+
 rm -fr ~/.vim
 mkdir ~/.vim
 echo "Created ~/.vim"
 
 git clone https://github.com/Shougo/neocomplcache.git ~/.vim
 echo "Installed neocomplcache"
+
+rm -fr ~/aws
+cp -fR aws ~/aws
+export PATH=$PATH:~/aws/s3cmd
+echo "Installed aws tools"
