ccf99c59468270f410e54ec626299a55539bb6ad
yuta <imai.factory@gmail.com>
2013-05-07 06:43:08 +0000

    emr

--- setup.sh
+++ setup.sh
@@ -19,6 +19,13 @@ echo "Created ~/.vim"
 git clone https://github.com/Shougo/neocomplcache.git ~/.vim
 echo "Installed neocomplcache"
 
+# prepare aws tools
+cd homedirectory
+wget http://elasticmapreduce.s3.amazonaws.com/elastic-mapreduce-ruby.zip
+unzip -d aws/emrclient elastic-mapreduce-ruby.zip
+rm elastic-mapreduce-ruby.zip
+cd ~/
+
 rm -fr ~/aws
 cp -fR ~/homedirectory/aws ~/aws
 echo "Installed aws tools"
